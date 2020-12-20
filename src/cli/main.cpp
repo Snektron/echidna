7ac1f8734d52c87fb746b0d4eda9d430510ab69f
Marcel <marcelhuijben@gmail.com>
2020-12-20 17:55:53 +0100

    CLI query functions

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
 #include <cstring>
 #include <cstdint>
+#include <memory>
+#include <vector>
 
 #include "net/socket.hpp"
 #include "protocol/cli.hpp"
 
-char shader_source[] = "OEFAUWIEPYTHON";
+struct JobStatus {
+    uint32_t id;
+    uint32_t frames_rendered;
+    uint32_t total_frames;
+};
 
-int main(int argc, char* argv[]) {
-    echidna::net::Socket socket;
+void submit_job(echidna::net::Socket& sock, const std::string& shader, uint32_t frames, uint32_t framerate, uint32_t width, uint32_t height) {
+    uint32_t shader_size = shader.size();
 
-    std::cout << "Connecting to server" << std::endl;
-    socket.connect("localhost", 4243);
-
-    uint32_t framerate = 42000;
-    uint32_t num_frames = 20;
-    uint32_t width = 420;
-    uint32_t height = 421;
-    uint32_t shader_size = sizeof(shader_source);
-
-    size_t buffer_size = 1 + 5 * sizeof(uint32_t) + sizeof(shader_source);
-    uint8_t* buffer = new uint8_t[buffer_size];
+    size_t buffer_size = 1 + 5 * sizeof(uint32_t) + shader.size();
+    std::unique_ptr<uint8_t[]> buffer(new uint8_t[buffer_size]);
     buffer[0] = static_cast<uint8_t>(echidna::protocol::CLIRequestID::UPLOAD);
     std::memcpy(&buffer[1], &framerate, sizeof(uint32_t));
-    std::memcpy(&buffer[5], &num_frames, sizeof(uint32_t));
+    std::memcpy(&buffer[5], &frames, sizeof(uint32_t));
     std::memcpy(&buffer[9], &width, sizeof(uint32_t));
     std::memcpy(&buffer[13], &height, sizeof(uint32_t));
     std::memcpy(&buffer[17], &shader_size, sizeof(uint32_t));
-    std::memcpy(&buffer[21], &shader_source, sizeof(shader_source));
+    std::memcpy(&buffer[21], shader.data(), shader_size);
 
-    std::cout << "Sending source" << std::endl;
+    sock.sendFully(buffer.get(), buffer_size);
+}
 
-    socket.sendFully(buffer, buffer_size);
-    delete[] buffer;
+std::vector<JobStatus> query_status(echidna::net::Socket& sock) {
+    echidna::protocol::CLIRequestID request = echidna::protocol::CLIRequestID::STATUS;
+    sock.sendFully(&request, sizeof(request));
 
-    std::cout << "Send completed" << std::endl;
+    std::vector<JobStatus> result;
+    uint32_t num_jobs = sock.recv<uint32_t>();
+    for(size_t i = 0; i < num_jobs; ++i) {
+        JobStatus s;
+        s.id = sock.recv<uint32_t>();
+        s.frames_rendered = sock.recv<uint32_t>();
+        s.total_frames = sock.recv<uint32_t>();
+        result.push_back(s);
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    echidna::net::Socket socket;
+    socket.connect("localhost", 4243);
+
+    submit_job(socket, "DIT IS EEN SHADER", 100, 42, 100, 100);
 
     return 0;
 }
